687975dfde36152cb07b700c74dedc0da6ff68ba
A.Grebnev <a.grebnev@roseltorg.ru>
2021-02-23 13:45:02 +0300

    new commit

---
+++ frontend/src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const makeApiRequest = () => {
+    axios.get('/api/testwithcurrentuser').then(response => {
+      console.log(response.data);
+    });
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code>sdfsdf
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <button onClick={makeApiRequest}>Make api request</button>
+    </div>
+  );
+}
+
+export default App;
